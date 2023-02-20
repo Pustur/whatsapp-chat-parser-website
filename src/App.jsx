b455b7436fa9ae1c909ad7910c3eaa56cb321be0
Davide Ciulla <davide.ciulla@hotmail.com>
2023-02-20 17:35:05 +0100

    Rendering sidebar only when there are messages to display

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
   rawFileAtom,
   participantsAtom,
   messagesDateBoundsAtom,
+  messagesAtom,
 } from './stores/global';
 import { datesAtom, globalFilterModeAtom, limitsAtom } from './stores/filters';
 import Dropzone from './components/Dropzone/Dropzone';
@@ -22,6 +23,7 @@ function App() {
   const [activeUser, setActiveUser] = useAtom(activeUserAtom);
   const [limits, setLimits] = useAtom(limitsAtom);
   const [dates, setDates] = useAtom(datesAtom);
+  const messages = useAtomValue(messagesAtom);
   const messagesDateBounds = useAtomValue(messagesDateBoundsAtom);
   const setRawFile = useSetAtom(rawFileAtom);
   const participants = useAtomValue(participantsAtom);
@@ -94,113 +96,115 @@ function App() {
           startDateInputString={dates.start}
           endDateInputString={dates.end}
         />
-        <Sidebar>
-          <S.Fieldset>
-            <legend>Filter by</legend>
-            {['index', 'date'].map(name => (
-              <S.RadioField key={name}>
-                <S.RadioInput
-                  id={name}
-                  name={name}
-                  type="radio"
-                  value={name}
-                  checked={filterMode === name}
-                  onChange={e => setFilterMode(e.target.value)}
-                />
-                <S.Label htmlFor={name}>
-                  {name.charAt(0).toUpperCase() + name.slice(1)}
-                </S.Label>
-              </S.RadioField>
-            ))}
-          </S.Fieldset>
-          {filterMode === 'index' && (
-            <S.Form onSubmit={setMessageLimits}>
-              <S.Fieldset>
-                <legend>Messages limit</legend>
-                <S.Field>
-                  <S.Label htmlFor="lower-limit">Start</S.Label>
-                  <S.Input
-                    id="lower-limit"
-                    name="lowerLimit"
-                    type="number"
-                    min="1"
-                    placeholder={limits.low}
+        {messages.length > 0 && (
+          <Sidebar>
+            <S.Fieldset>
+              <legend>Filter by</legend>
+              {['index', 'date'].map(name => (
+                <S.RadioField key={name}>
+                  <S.RadioInput
+                    id={name}
+                    name={name}
+                    type="radio"
+                    value={name}
+                    checked={filterMode === name}
+                    onChange={e => setFilterMode(e.target.value)}
                   />
-                </S.Field>
-                <S.Field>
-                  <S.Label htmlFor="upper-limit">End</S.Label>
-                  <S.Input
-                    id="upper-limit"
-                    name="upperLimit"
-                    type="number"
-                    min="1"
-                    placeholder={limits.high}
-                  />
-                </S.Field>
-                <S.Field>
-                  <S.Submit type="submit" value="Apply" />
-                  <S.InputDescription>
-                    A high delta may freeze the page for a while, change this
-                    with caution
-                  </S.InputDescription>
-                </S.Field>
-              </S.Fieldset>
-            </S.Form>
-          )}
-          {filterMode === 'date' && (
-            <S.Form onSubmit={setMessagesByDate}>
-              <S.Fieldset>
-                <legend>Messages date window</legend>
-                <S.Field>
-                  <S.Label htmlFor="start-date">Start</S.Label>
-                  <S.Input
-                    id="start-date"
-                    name="startDate"
-                    type="date"
-                    min={messagesDateBounds.start}
-                    max={messagesDateBounds.end}
-                    defaultValue={messagesDateBounds.start}
-                  />
-                </S.Field>
-                <S.Field>
-                  <S.Label htmlFor="end-date">End</S.Label>
-                  <S.Input
-                    id="end-date"
-                    name="endDate"
-                    type="date"
-                    min={messagesDateBounds.start}
-                    max={messagesDateBounds.end}
-                    defaultValue={messagesDateBounds.end}
-                  />
-                </S.Field>
-                <S.Field>
-                  <S.Submit type="submit" value="Apply" />
-                  <S.InputDescription>
-                    A high delta may freeze the page for a while, change this
-                    with caution
-                  </S.InputDescription>
-                </S.Field>
-              </S.Fieldset>
-            </S.Form>
-          )}
-          <S.Field>
-            <S.Label htmlFor="active-user">Active User</S.Label>
-            <S.Select
-              id="active-user"
-              disabled={!participants.length}
-              value={activeUser}
-              onChange={e => {
-                setActiveUser(e.target.value);
-              }}
-            >
-              {participants.map(participant => (
-                <option key={participant} value={participant}>
-                  {participant}
-                </option>
+                  <S.Label htmlFor={name}>
+                    {name.charAt(0).toUpperCase() + name.slice(1)}
+                  </S.Label>
+                </S.RadioField>
               ))}
-            </S.Select>
-          </S.Field>
-        </Sidebar>
+            </S.Fieldset>
+            {filterMode === 'index' && (
+              <S.Form onSubmit={setMessageLimits}>
+                <S.Fieldset>
+                  <legend>Messages limit</legend>
+                  <S.Field>
+                    <S.Label htmlFor="lower-limit">Start</S.Label>
+                    <S.Input
+                      id="lower-limit"
+                      name="lowerLimit"
+                      type="number"
+                      min="1"
+                      placeholder={limits.low}
+                    />
+                  </S.Field>
+                  <S.Field>
+                    <S.Label htmlFor="upper-limit">End</S.Label>
+                    <S.Input
+                      id="upper-limit"
+                      name="upperLimit"
+                      type="number"
+                      min="1"
+                      placeholder={limits.high}
+                    />
+                  </S.Field>
+                  <S.Field>
+                    <S.Submit type="submit" value="Apply" />
+                    <S.InputDescription>
+                      A high delta may freeze the page for a while, change this
+                      with caution
+                    </S.InputDescription>
+                  </S.Field>
+                </S.Fieldset>
+              </S.Form>
+            )}
+            {filterMode === 'date' && (
+              <S.Form onSubmit={setMessagesByDate}>
+                <S.Fieldset>
+                  <legend>Messages date window</legend>
+                  <S.Field>
+                    <S.Label htmlFor="start-date">Start</S.Label>
+                    <S.Input
+                      id="start-date"
+                      name="startDate"
+                      type="date"
+                      min={messagesDateBounds.start}
+                      max={messagesDateBounds.end}
+                      defaultValue={messagesDateBounds.start}
+                    />
+                  </S.Field>
+                  <S.Field>
+                    <S.Label htmlFor="end-date">End</S.Label>
+                    <S.Input
+                      id="end-date"
+                      name="endDate"
+                      type="date"
+                      min={messagesDateBounds.start}
+                      max={messagesDateBounds.end}
+                      defaultValue={messagesDateBounds.end}
+                    />
+                  </S.Field>
+                  <S.Field>
+                    <S.Submit type="submit" value="Apply" />
+                    <S.InputDescription>
+                      A high delta may freeze the page for a while, change this
+                      with caution
+                    </S.InputDescription>
+                  </S.Field>
+                </S.Fieldset>
+              </S.Form>
+            )}
+            <S.Field>
+              <S.Label htmlFor="active-user">Active User</S.Label>
+              <S.Select
+                id="active-user"
+                disabled={!participants.length}
+                value={activeUser}
+                onChange={e => {
+                  setActiveUser(e.target.value);
+                }}
+              >
+                {participants.map(participant => (
+                  <option key={participant} value={participant}>
+                    {participant}
+                  </option>
+                ))}
+              </S.Select>
+            </S.Field>
+          </Sidebar>
+        )}
       </S.Container>
     </>
   );
