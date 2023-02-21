8dbe06812c8cb2e19c458585dc95f931d9258408
Davide Ciulla <davide.ciulla@hotmail.com>
2023-02-21 15:47:10 +0100

    Extracted dates form and active user selector

---
+++ src/components/Sidebar/ActiveUserSelector.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import * as S from './style';
+
+function ActiveUserSelector({ participants, activeUser, setActiveUser }) {
+  return (
+    <S.Field>
+      <S.Label htmlFor="active-user">Active User</S.Label>
+      <S.Select
+        id="active-user"
+        disabled={!participants.length}
+        value={activeUser}
+        onChange={e => {
+          setActiveUser(e.target.value);
+        }}
+      >
+        {participants.map(participant => (
+          <option key={participant} value={participant}>
+            {participant}
+          </option>
+        ))}
+      </S.Select>
+    </S.Field>
+  );
+}
+
+export default ActiveUserSelector;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,10 @@ import React, { useRef, useEffect, useState } from 'react';
 import { useAtom, useAtomValue, useSetAtom } from 'jotai';
 
 import Credits from '../Credits/Credits';
+import FilterModeSelector from './FilterModeSelector';
+import FilterMessageLimitsForm from './FilterMessageLimitsForm';
+import FilterMessageDatesForm from './FilterMessageDatesForm';
+
 import * as S from './style';
 import {
   activeUserAtom,
@@ -9,14 +13,12 @@ import {
   messagesDateBoundsAtom,
   participantsAtom,
 } from '../../stores/global';
-import { getISODateString } from '../../utils/utils';
 import {
   datesAtom,
   globalFilterModeAtom,
   limitsAtom,
 } from '../../stores/filters';
-import FilterModeSelector from './FilterModeSelector';
-import FilterMessageLimitsForm from './FilterMessageLimitsForm';
+import ActiveUserSelector from './ActiveUserSelector';
 
 function Sidebar() {
   const [isMenuOpen, setIsMenuOpen] = useAtom(isMenuOpenAtom);
@@ -99,58 +101,16 @@ function Sidebar() {
               />
             )}
             {filterMode === 'date' && (
-              <S.Form onSubmit={setMessagesByDate}>
-                <S.Fieldset>
-                  <legend>Messages date window</legend>
-                  <S.Field>
-                    <S.Label htmlFor="start-date">Start</S.Label>
-                    <S.Input
-                      id="start-date"
-                      name="startDate"
-                      type="date"
-                      min={getISODateString(messagesDateBounds.start)}
-                      max={getISODateString(messagesDateBounds.end)}
-                      defaultValue={getISODateString(messagesDateBounds.start)}
-                    />
-                  </S.Field>
-                  <S.Field>
-                    <S.Label htmlFor="end-date">End</S.Label>
-                    <S.Input
-                      id="end-date"
-                      name="endDate"
-                      type="date"
-                      min={getISODateString(messagesDateBounds.start)}
-                      max={getISODateString(messagesDateBounds.end)}
-                      defaultValue={getISODateString(messagesDateBounds.end)}
-                    />
-                  </S.Field>
-                  <S.Field>
-                    <S.Submit type="submit" value="Apply" />
-                    <S.InputDescription>
-                      A high delta may freeze the page for a while, change this
-                      with caution
-                    </S.InputDescription>
-                  </S.Field>
-                </S.Fieldset>
-              </S.Form>
+              <FilterMessageDatesForm
+                messagesDateBounds={messagesDateBounds}
+                setMessagesByDate={setMessagesByDate}
+              />
             )}
-            <S.Field>
-              <S.Label htmlFor="active-user">Active User</S.Label>
-              <S.Select
-                id="active-user"
-                disabled={!participants.length}
-                value={activeUser}
-                onChange={e => {
-                  setActiveUser(e.target.value);
-                }}
-              >
-                {participants.map(participant => (
-                  <option key={participant} value={participant}>
-                    {participant}
-                  </option>
-                ))}
-              </S.Select>
-            </S.Field>
+            <ActiveUserSelector
+              participants={participants}
+              activeUser={activeUser}
+              setActiveUser={setActiveUser}
+            />
           </S.SidebarChildren>
           <Credits />
         </S.SidebarContainer>
