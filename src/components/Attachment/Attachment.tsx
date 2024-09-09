7e33745b5e5ee0433dd7830aeadc6d93847e6d73
Loris Bettazza <loris.bettazza@gmail.com>
2024-09-09 23:10:50 +0200

    :goal_net: Handle big files that can't be blobbed by JSZip
    And by handle I mean, show an error that it can't be done
    
    Related to: #42

--- src/components/Attachment/Attachment.tsx
+++ src/components/Attachment/Attachment.tsx
@@ -57,9 +57,21 @@ function Attachment({ fileName }: IAttachment) {
       return;
     }
 
-    file.async('blob').then(blob => {
-      if (isMounted()) setAttachment(URL.createObjectURL(blob));
-    });
+    const sizeLimit = 250 * 1024 * 1024; // 250 MB
+    // @ts-expect-error _data is not typed
+    // eslint-disable-next-line no-underscore-dangle
+    const uncompressedSize = file?._data?.uncompressedSize ?? -1;
+
+    // We actually need to check > 0 because big files have negative numbers (int overflow? kek)
+    if (uncompressedSize > 0 && uncompressedSize < sizeLimit) {
+      // TODO: Maybe we should generate this crap only on user request, otherwise it lingers in memory needlessly
+      file.async('blob').then(blob => {
+        if (isMounted()) setAttachment(URL.createObjectURL(blob));
+      });
+      return;
+    }
+
+    setError(new Error(`Can't load "${fileName}" as it exceeds 250MB`));
   }, [extractedFile, fileName, isMounted]);
 
   if (error) return <div>{error.toString()}</div>;
